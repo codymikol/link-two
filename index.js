eae4bb08d925fb1acd0279c2d41b7644ec237c8a
csecskedics <csaba.csecskedi@possible.com>
2016-06-09 15:21:35 +0200

    sandbox

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 "use strict";
 
-var http = require("http"),
-    express = require("express"),
-    socketio = require("socket.io"),
-    game = require("./dist/server");
+function createSandbox() {
+    var sandbox = Object.create(null);
+	Object.defineProperty(sandbox, 'module', {
+		enumerable: true,
+		configurable: false,
+		writable: false,
+		value: Object.create(null)
+	});
+	sandbox.module.exports = Object.create(null);
+	sandbox.exports = sandbox.module.exports;
+    sandbox.console = console;
+    sandbox.setTimeout = setTimeout;
+    sandbox.setInterval = setInterval;
+    sandbox.clearTimeout = clearTimeout;
+    sandbox.clearInterval = clearInterval;
+	return sandbox;
+};
 
-var app = express(),
-    server = http.Server(app),
-    io = socketio(server);
+require('fs').readFile('./dist/server.js', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
 
-app.set('port', (process.env.PORT || 3000));
+    var express = require("express"),
+        app = express(),
+        server = require("http").Server(app),
+        io = require("socket.io")(server),
+        sandbox = createSandbox();
 
-app.use(express.static('dist'));
-
-io.on('connection', game);
-
-server.listen(app.get('port'), function () {
-    console.log("Server started");
+	require('vm').runInNewContext(data, sandbox);
+    io.on('connection', sandbox.module.exports);
+    app.set('port', (process.env.PORT || 3000));
+    app.use(express.static('dist'));
+    server.listen(app.get('port'), function () {
+        console.log("Server started");
+    });
 });
