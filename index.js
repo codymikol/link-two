b3b433e7643c554033fcff5cf7270b0a96d0847b
csecskedics <csaba.csecskedi@possible.com>
2018-06-29 17:01:11 +0200

    Sequelize storage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const createSandbox = () => {
+function createSandbox() {
   const sandbox = {
     console,
     setTimeout,
@@ -17,40 +17,20 @@ const createSandbox = () => {
   });
   sandbox.module.exports = Object.create(null);
   sandbox.exports = sandbox.module.exports;
-
-  exposeRemoteServices(sandbox);
-
   return sandbox;
-};
-
-const exposeRemoteServices = (sandbox) => {
-  if (process.env.GLITCHD_TOKEN === undefined) {
-    return
-  }
-
-  const services = require('glitchd-client-node');
-  sandbox.Buffer = Buffer;
-  Object.defineProperty(sandbox, 'glitchd', {
-    value: Object.create(null, {
-      items: {
-        value: new services.ItemsStore('services.js13kgames.com:13312', process.env.GLITCHD_TOKEN)
-      }
-    })
-  });
 };
 
 require('fs').readFile('./public/shared.js', 'utf8', (err, shared) => {
   require('fs').readFile('./public/server.js', 'utf8', (err, code) => {
     if (err) {
-      throw err
+      throw err;
     }
 
-    const
-      express = require('express'),
-      app     = express(),
-      server  = require('http').Server(app),
-      io      = require('socket.io')(server),
-      sandbox = createSandbox();
+    const express = require('express');
+    const app = express();
+    const server = require('http').Server(app);
+    const io = require('socket.io')(server);
+    const sandbox = createSandbox();
 
     require('vm').runInNewContext(shared + '\n' + code, sandbox);
     io.on('connection', sandbox.module.exports);
