1d6565e32f737ce1a79aea47fd37e8e5a6910ee1
csecskedics <csaba.csecskedi@possible.com>
2018-07-23 14:26:07 +0200

    body-parser & server-info

--- index.js
+++ index.js
@@ -27,14 +27,18 @@ const createSandbox = () => {
 
 storage.init(process.env.DATABASE_URL || 'sqlite:storage.sqlite').then(() => {
     const express = require('express');
+    const parser = require('body-parser');
     const app = express();
     const server = require('http').Server(app);
     const io = require('socket.io')(server);
     const sandbox = createSandbox();
     require('vm').runInNewContext(shared + '\n' + code, sandbox);
+
     if (typeof sandbox.module.exports == 'function') {
         io.on('connection', sandbox.module.exports);
     } else if (typeof sandbox.module.exports == 'object') {
+        app.use(parser.json())
+        app.use(parser.urlencoded({ extended: true }));
         for (let route in sandbox.module.exports) {
             if (route == 'io') {
                 io.on('connection', sandbox.module.exports[route]);
@@ -43,6 +47,15 @@ storage.init(process.env.DATABASE_URL || 'sqlite:storage.sqlite').then(() => {
             }
         }
     }
+
+    app.get('/server-info', (req, res) => {
+        let limit = 13312,
+            storageSize = storage.interface.size();
+        res.set('Content-Type', 'text/plain').send([
+            `Storage: ${storageSize} byte / ${storageSize ? limit / storageSize * 100 : 0}%`
+        ].join("\n"));
+    });
+
     app.set('port', (process.env.PORT || 3000));
     app.use(express.static('public'));
     server.listen(app.get('port'), () => {
