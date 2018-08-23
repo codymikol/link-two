f4b63162b2303236d5a8ca1d0634f25c6f694e9d
John Flynn <john.flynn@boltstech.com>
2018-08-23 00:19:17 -0400

    fleshed out concept of Room and players more. added asDTO concept for not sending socket back to the client. added rough startGame method if 2 people have joined.

--- public/server.js
+++ public/server.js
@@ -2,75 +2,114 @@
 
 const rooms = [];
 
+const required_players = 2;
+
 class Room {
 
-	constructor(index, socket) {
-		this.id = index;
-		this.roomName = 'Room #' + (index+1);
-		this.players = [];
-	}
+    constructor(index) {
+        this.id = index;
+        this.roomName = 'Room #' + (index + 1);
+        this.players = [];
+    }
 
-	join(player) {
-		this.players.push(player);
-		console.log('User: ' + player.name + ' has joined: ' + this.roomName);
-	}
+    join(player) {
+        console.log(player)
+        this.players.push(player);
+        console.log('User: ' + player.name + ' has joined: ' + this.roomName);
+        if (player.length === required_players) {
+            setInterval(this.startGame, 3)
+        }
+    }
+
+    startGame() {
+        this.players.forEach(this.broadcastPlayer)
+    }
+
+    broadcastPlayer(player, index) {
+        player.socket.emit("player-info", players.map(function (player) {
+            return {name: player.name, health: player.health, x: player.x, y: player.y}
+        }))
+    }
 
 
-	leave(player){
-		this.players = this.players.filter(function (mPlayer) {
-			return player !== mPlayer;
+    leave(player) {
+        this.players = this.players.filter(function (mPlayer) {
+            return player !== mPlayer;
         });
-	}
+    }
+
+    asDTO() {
+        return {
+            players: this.players.map(function (player) {
+                return player.asDTO()
+            }),
+            roomName: this.roomName
+        }
+    }
 
 }
 
 class Player {
-	constructor() {
-      this.name = 'cody mikol';
-	}
+
+    constructor(socket) {
+        this.x = 0;
+        this.y = 0;
+        this.health = 100;
+        this.socket = socket;
+        this.name = 'cody mikol';
+    }
+
+    asDTO() {
+        return {name : this.name, health : this.health, x : this.x, y : this.y}
+    }
+
 }
 
 function init() {
-    for(let i = 0; i < 10; i++) {rooms.push(new Room(i));}
+    for (let i = 0; i < 10; i++) {
+        rooms.push(new Room(i));
+    }
 }
 
 init();
 
 module.exports = {
 
-	io: (socket) => {
+    io: (socket) => {
 
-		const player = new Player(socket);
-		let chosenRoom = null;
+        const player = new Player(socket);
+        var theRoom = null;
 
-		socket.emit("rooms-available", rooms);
 
-		socket.on("join", function (room) {
-			var theRoom = rooms.filter(function (mRoom) {
-				return room.roomName === mRoom.roomName;
+        socket.emit("rooms-available", rooms.map(function (room) {
+            return room.asDTO();
+        }));
+
+        socket.on("join", function (room) {
+            theRoom = rooms.filter(function (mRoom) {
+                return room.roomName === mRoom.roomName;
             })[0];
 
-			chosenRoom = theRoom;
+            theRoom.join(player);
 
-			chosenRoom.join(player);
+            socket.emit('joined-room');
 
-			socket.emit('joined-room');
-            socket.broadcast.emit('update-rooms', rooms);
+            socket.broadcast.emit('update-rooms', rooms.map(function (room) {
+                return room.asDTO();
+            }));
+
             console.log("Connected: " + socket.id);
+
         });
 
         socket.on("disconnect", () => {
-        	if(chosenRoom) chosenRoom.leave(player);
-            socket.broadcast.emit('update-rooms', rooms);
+
+            theRoom.leave(player);
+
+            socket.broadcast.emit('update-rooms', rooms.map(function (room) {
+                return room.asDTO();
+            }));
         });
 
-
     },
-
-	stat: (req, res) => {
-		// storage.get('games', 0).then(games => {
-		// 	res.send(`<h1>Games played: ${games}</h1>`);
-		// });
-	}
-
 };
