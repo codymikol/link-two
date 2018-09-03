8f33891ad9ac0681d0c89b5e3648e5c0b7d20806
John Flynn <john.flynn@boltstech.com>
2018-09-03 16:55:10 -0400

    removed RoomList as it is not needed.

--- public/server.js
+++ public/server.js
@@ -3,53 +3,7 @@
 let rooms = [];
 let playerNonce = 0;
 let projectileNonce = 0;
-//
-// class RoomList {
-//
-//     constructor() {
-//
-//         this.rooms = [];
-//
-//         this.add = function (room) {
-//             this.rooms.push(room)
-//         };
-//         this.bynonce = function (nonce) {
-//             return this.rooms.filter(function (room) {
-//                 return room.nonce === nonce;
-//             })[0]
-//         };
-//         this.asDTO = function () {
-//             return this.rooms.reduce(function (col, room) {
-//                 col[room.nonce] = room.asDTO();
-//                 return col;
-//             }, {})
-//         };
-//
-//         this.bestRoom = function () {
-//             // let openRooms = this.rooms.filter(room => (room.environment.players.length < room.maxPlayers));
-//             // let bestRoom = openRooms[0];
-//             // for (let i = 0; i < openRooms.length; i++) {
-//             //     if (openRooms[i].map.players.length > bestRoom.map.players.length) {
-//             //         bestRoom = openRooms[i];
-//             //     }
-//             // }
-//             return this.rooms[0];
-//         };
-//
-//         this.updateRoom = function (room) {
-//             if (room.environment.players.length > 0) {
-//                 room._roomTick();
-//                 io.in('room_' + room.nonce).volatile.emit('update-chosen-room', room.asDTO(true))
-//             }
-//         };
-//
-//         this.serverTick = function () {
-//             serverTime = Date.now();
-//             this.rooms.forEach(this.updateRoom);
-//         };
-//
-//     }
-// }
+
 
 class Room {
 
