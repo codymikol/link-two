f9e350349560d0f995dceafde3f4e2a243a708b9
Cody Mikol <cody.mikol@boltstech.com>
2018-09-09 16:53:32 -0400

    Added a Stats class for tracking payer progress throughout a game.

--- public/server.js
+++ public/server.js
@@ -67,6 +67,72 @@ class Room extends Entity {
     }
 }
 
+class Stats {
+    constructor(player) {
+        this.roundHits = 0;
+        this.roundMisses = 0;
+        this.roundKills = 0;
+        this.roundWon = null;
+        this.totalWins = 0;
+        this.totalHits = 0;
+        this.totalMisses = 0;
+        this.totalKills = 0;
+        this.totalDeaths = 0;
+    }
+
+    awardHit() {
+        this.roundHits++;
+        this.totalHits++;
+    }
+
+    awardMiss() {
+        this.roundMisses++;
+        this.totalMisses++;
+    }
+
+    awardRoundWin() {
+        this.roundWon = true;
+        this.totalWins++;
+    }
+
+    awardKill() {
+        this.roundKills++;
+        this.totalKills++;
+    }
+
+    awardDeath() {
+        this.roundWon = false;
+        this.totalDeaths++;
+    }
+
+    resetRoundStats() {
+        this.roundHits = 0;
+        this.roundMisses = 0;
+        this.roundKills = 0;
+        this.roundWon = null;
+    }
+
+    getRoundStats() {
+        return {
+            roundHits: this.roundHits,
+            roundMisses: this.roundMisses,
+            roundKills: this.roundKills,
+            roundWon: this.roundWon
+        }
+    }
+
+    getGameStats() {
+        return {
+            totalWins: this.totalWins,
+            totalHits: this.totalHits,
+            totalMisses: this.totalMisses,
+            totalKills: this.totalKills,
+            totalDeaths: this.totalDeaths
+        }
+    }
+
+}
+
 function serverTick() {
     serverTime = Date.now();
     rooms.forEach(function (room) {
