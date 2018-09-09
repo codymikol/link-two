354c9df3da6d51ba8301ff1f95f086618e92e1a7
Cody Mikol <cody.mikol@boltstech.com>
2018-09-09 16:52:54 -0400

    Actually use the requiredPlayers field

--- public/server.js
+++ public/server.js
@@ -28,7 +28,7 @@ class Room extends Entity {
     leave(playerNonce) {
         this.emit('destroy', 'enemy-' + playerNonce);
         this.environment.actors.delete(playerNonce);
-        if (this.environment.actors.size < 2) delete this.countdown;
+        if (this.environment.actors.size < this.requiredPlayers) delete this.countdown;
     }
 
     startGame() {
